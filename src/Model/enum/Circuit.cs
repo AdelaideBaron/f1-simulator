a4a0542cd3c0c9faaaeb34eb11ec7e1e422cbf35
AdelaideBaron <baronadelaide@hotmail.com>
2024-03-25 18:03:48 +0000

    add coordinates to circuit

--- src/Model/enum/Circuit.cs
+++ src/Model/enum/Circuit.cs
@@ -6,21 +6,37 @@ public class CircuitNameAttribute(string value) : Attribute
     public string Value { get; } = value;
 }
 
+[AttributeUsage(AttributeTargets.Field)]
+public class CircuitCoordinatesAttribute : Attribute
+{
+    public double Longitude { get; }
+    public double Latitude { get; }
+
+    public CircuitCoordinatesAttribute(double longitude, double latitude)
+    {
+        Longitude = longitude;
+        Latitude = latitude;
+    }
+}
+
 public enum Circuit
 {
+    [CircuitCoordinates(9.28, 45.62)] 
     [CircuitName("Autodromo Nazionale di Monza")]
     Monza,
 
+    [CircuitCoordinates(50.51, 26.03)] 
     [CircuitName("Bahrain International Circuit")]
     Bahrain,
-    
+
+    [CircuitCoordinates(-1.01, 52.07)] 
     [CircuitName("Silverstone Circuit")]
     Silverstone,
 
+    [CircuitCoordinates(7.41, 43.73)] 
     [CircuitName("Circuit de Monaco")]
     Monaco
 }
-
 public static class CircuitExtensions
 {
     public static string GetCircuitName(this Enum enumValue)
@@ -29,5 +45,12 @@ public static class CircuitExtensions
         var field = enumType.GetField(enumValue.ToString());
         var attribute = (CircuitNameAttribute)Attribute.GetCustomAttribute(field, typeof(CircuitNameAttribute));
         return attribute?.Value; // Todo warning about null
+    }
+    
+    public static CircuitCoordinatesAttribute GetCircuitCoordinates(this Circuit enumValue)
+    {
+        var enumType = enumValue.GetType();
+        var field = enumType.GetField(enumValue.ToString());
+        return (CircuitCoordinatesAttribute)Attribute.GetCustomAttribute(field, typeof(CircuitCoordinatesAttribute));
     }
 }
--- tests/ModelTests/entity/CircuitTests.cs
+++ tests/ModelTests/entity/CircuitTests.cs
@@ -10,4 +10,12 @@ public class CircuitTests
         string actual = CircuitExtensions.GetCircuitName(Circuit.Bahrain);
         Assert.That(actual, Is.EqualTo("Bahrain International Circuit"));
     }
+    
+    [Test]
+    public void CircuitExtensionsRetrievesExpectedCoordinates()
+    {
+        CircuitCoordinatesAttribute actual = CircuitExtensions.GetCircuitCoordinates(Circuit.Monza);
+        Assert.That(actual.Latitude, Is.EqualTo(45.62));
+        Assert.That(actual.Longitude, Is.EqualTo(9.28));
+    }
 }
